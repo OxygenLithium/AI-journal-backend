6d2eae394caf6166004c03ccd4e48c9630713c4d
Oliver Mao <olivermao2005@gmail.com>
2025-04-28 03:12:34 -0400

    Added ability for front end to speak to Cohere

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
 require('dotenv').config();
+const { CohereClientV2 } = require('cohere-ai');
 
-const app = require('express')();
+const express = require('express');
+const app = express();
 const port = 3000;
+app.use(express.json());
 
-console.log(process.env.API_KEY);
+const cohere = new CohereClientV2({
+    token: process.env.API_KEY,
+});
 
 // Start the server
 app.listen(port, () => {
     console.log(`Node.js HTTP server is running on port ${port}`);
 });
 
-app.get('/test', (req, res) => {
+app.post('/test', async (req, res) => {
+    console.log(req.body);
+    const rawResponse = await cohere.chat({
+        model: 'command-a-03-2025',
+        messages: [
+          {
+            role: 'user',
+            content: req.body.input,
+          },
+        ],
+    });
+    const response = rawResponse.message.content[0].text;
+    console.log(response);
+
     res.status(200).send({
-        test: "successful"
+        cohereResponse: response
     })
 })
